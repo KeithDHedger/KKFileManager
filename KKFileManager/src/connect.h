c9fc951f1cc7266b6316f63749634972c057d4e8
K D Hedger <kdhedger68713@gmail.com>
2017-02-24 14:16:39 +0000

    Added templates to menu, templates are in ~/Tempplates

--- KKFileManager/src/connect.h
+++ KKFileManager/src/connect.h
@@ -1,11 +1,22 @@
-/******************************************************
-*
-*     ©keithhedger Thu 23 Feb 16:14:23 GMT 2017
-*     kdhedger68713@gmail.com
-*
-*     connect.h
-* 
-******************************************************/
+/*
+ *
+ * ©K. D. Hedger. Fri 24 Feb 14:14:54 GMT 2017 kdhedger68713@gmail.com
+
+ * This file (connect.h) is part of KKFileManager.
+
+ * KKFileManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * KKFileManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with KKFileManager.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <gtk/gtk.h>
 
